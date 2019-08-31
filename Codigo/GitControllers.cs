d06ad1e92731c5a5ab1dac3eb93370fedc7bca3b
Jose Guerrero <jose_guerrero5@usmp.pe>
2019-08-31 10:59:38 -0500

    Pep

---
+++ Codigo/GitControllers.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace  muynamespace
+{
+    class MyClass
+    {
+        public String Name {get;set};
+        public String Age {get;set};
+        public String Apellido {get;set};
+    }
+}
+{
+    
+}
